4620f7c619a330a43482cc8192ef030e433e2f91
Lierian <lierian.ua@gmail.com>
2017-09-18 19:52:14 +0300

    new2

--- medbase/settings.py
+++ medbase/settings.py
@@ -114,7 +114,9 @@ DATABASES = {
     }
 }
 
-
+ALLOWED_HOSTS = ['*']
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -153,5 +155,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
